df60accf288ac9cb262d1b62818883a73317a111
sudiplo <leosudip949@gmail.com>
2025-12-13 10:34:21 +0545

    modify updata password form

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -15,19 +15,19 @@
 
         <div>
             <label for="update_password_current_password" :value="__('Current Password')">Current Password</label>
-            <input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
+            <input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" required />
             <x-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
         </div>
 
         <div>
             <label for="update_password_password" :value="__('New Password')" >New Password</label>
-            <input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+            <input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" required/>
             <error :messages="$errors->updatePassword->get('password')" class="mt-2" />
         </div>
 
         <div>
             <label for="update_password_password_confirmation" :value="__('Confirm Password')">Confirm Password</label>
-            <input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+            <input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" required/>
             <x-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
         </div>
 
