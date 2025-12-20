3fd12efed46e5b8a38d0e256567a2d02924fd4c9
sudiplo <leosudip949@gmail.com>
2025-12-20 19:52:44 +0545

    reset password modify

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,32 +1,37 @@
 <x-front>
-    <div class="bg-[#efeff1] hover:bg-white p-20 rounded shadow-xl">
-        <form method="POST" action="{{ route('password.store') }}">
-            @csrf
+    <form method="POST" action="{{ route('password.store') }}" class="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full mx-auto space-y-6 transform transition-all duration-300 ease-in-out hover:scale-105">
+        @csrf
+        <h2 class="text-3xl font-semibold text-center text-gray-800">Reset Password</h2>
+        <p class="text-center text-gray-600 text-sm">Enter the email to reset your password</p>
+        <!-- Password Reset Token -->
+        <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <!-- Password Reset Token -->
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+        <div>
+            <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+            <input type="email" id="email" name="email" :value="old('email', $request->email)" class="w-full px-4 py-3 mt-2 bg-neutral-100 border border-gray-300 rounded-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400 transition-all" placeholder="name@example.com" required autofocus autocomplete="username" />
+            <x-error class="mt-2 text-red-500 text-sm" :messages="$errors->get('email')" />
+        </div>
 
-            <div class="mb-5">
-                <label for="email" :value="__('Email') " class="block mb-2.5 text-xl font-medium text-heading">Email</label>
-                <input type="email" id="email" name="email" :value="old('email', $request->email)" class="bg-neutral-secondary-medium border border-default-medium text-heading text-xl rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" required autofocus autocomplete="username" />
-            </div>
+        <div>
+            <label for="password" class="block text-sm font-medium text-gray-700">New Password</label>
+            <input type="password" id="password" class="block mt-1 w-full" name="password" class="w-full px-4 py-3 mt-2 bg-neutral-100 border border-gray-300 rounded-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400 transition-all" placeholder="••••••••" required autocomplete="new-password"/>
+        </div>
+        {{-- Confirm Password --}}
+         <div>
+            <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm Password</label>
+            <input type="password" id="password_confirmation" class="block mt-1 w-full" name="password_confirmation" class="w-full px-4 py-3 mt-2 bg-neutral-100 border border-gray-300 rounded-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400 transition-all" placeholder="••••••••" required autocomplete="new-password" />
+            <x-error class="mt-2 text-red-500 text-sm" :messages="$errors->get('password')" />
+        </div>
 
-            <div class="mt-5">
-                <label for="password" :value="__('Password')" class="block mb-2.5 text-xl font-medium text-heading" >Password</label>
-                <input type="password" id="password" class="block mt-1 w-full" name="password" required autocomplete="new-password" />
-            </div>
+        <div class="flex items-center">
+            <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded-sm bg-neutral-100 focus:ring-2 focus:ring-blue-300" required/>
+            <label for="remember" class="ml-2 text-sm text-gray-600">Remember me</label>
+        </div>
 
-            <!-- Confirm Password -->
-            <div class="mt-5">
-                <label for="password_confirmation" :value="__('Confirm Password')" class="block mb-2.5 text-xl font-medium text-heading">Confirm Password</label>
-                <input id="password_confirmation" class="block mt-1 w-full"
-                    type="password"
-                    name="password_confirmation" required autocomplete="new-password"/>
-            </div>
-            <div class="p-4">
-                <button class="bg-blue-500 text-white px-8 py-3 rounded shadow hover:bg-blue-600 transition flex items-center text-xl space-x-2"> Reset Password</button>
-            </div>
-        </form>
-    </div>
-
+        <div class="flex items-center justify-between mt-6">
+            <button type="submit" class="w-full bg-blue-600 text-white py-3 rounded-md shadow-md hover:bg-blue-700 focus:outline-none transition transform hover:scale-105">
+               Reset Password
+            </button>
+        </div>
+    </form>
 </x-front>
