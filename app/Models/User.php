9233f1048bf225a12301e91ce1fb4a6804ed9847
sudiplo <leosudip949@gmail.com>
2026-01-13 14:54:35 +0545

    relation between user and citizenship

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class User extends Authenticatable
 {
@@ -44,5 +45,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    // relation between user and citizen
+    public function citizen(): BelongsTo
+    {
+        return $this->belongsTo(citizenship::class);
     }
 }
--- app/Models/citizenship.php
+++ app/Models/citizenship.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class citizenship extends Model
 {
@@ -65,5 +66,11 @@ class citizenship extends Model
     public function wardCandidate(): HasMany
     {
         return $this->hasMany(wardCandidate::class);
+    }
+
+    // relation between citizen and user
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
     }
 }
